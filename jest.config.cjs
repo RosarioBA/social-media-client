d18cb31708e14bd980949ac6946cb34b9d8c9df6
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-10-16 20:42:38 +0200

    Add first tests

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.js$": ["babel-jest", { configFile: './.babelrc.json' }]
+  },
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+  transformIgnorePatterns: [],
+  moduleDirectories: ['node_modules', 'src'],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+};
---
+++ test/basic.test.js
@@ -0,0 +1,4 @@
+// basic.test.js
+test('basic test', () => {
+    expect(true).toBe(true);
+  });
