6d6578d9eed0faef2b10ec156bc432a8c5184cd2
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-10-17 22:15:30 +0200

    Fix Cypress tests to handle multiple login/logout buttons

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,11 +1,11 @@
 describe('Authentication', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5500'); // Make sure this URL is correct for your application
+    cy.visit('http://localhost:5500');
   });
 
   it('User can log in with the login form using valid credentials', () => {
     // Open the login modal
-    cy.get('button[data-auth="login"]').click();
+    cy.get('button[data-auth="login"]').first().click();
 
     // Wait for the modal to be visible
     cy.get('#loginModal').should('be.visible');
@@ -18,12 +18,12 @@ describe('Authentication', () => {
     cy.get('#loginForm').submit();
 
     // Check if the logout button is visible (indicating successful login)
-    cy.get('button[data-auth="logout"]').should('be.visible');
+    cy.get('button[data-auth="logout"]').first().should('be.visible');
   });
 
   it('User cannot submit the login form with invalid credentials and is shown an error message', () => {
     // Open the login modal
-    cy.get('button[data-auth="login"]').click();
+    cy.get('button[data-auth="login"]').first().click();
 
     // Wait for the modal to be visible
     cy.get('#loginModal').should('be.visible');
@@ -42,19 +42,19 @@ describe('Authentication', () => {
 
   it('User can log out with the logout button', () => {
     // First, log in
-    cy.get('button[data-auth="login"]').click();
+    cy.get('button[data-auth="login"]').first().click();
     cy.get('#loginModal').should('be.visible');
     cy.get('#loginEmail').type('validuser@noroff.no');
     cy.get('#loginPassword').type('validPassword123');
     cy.get('#loginForm').submit();
 
     // Verify login was successful
-    cy.get('button[data-auth="logout"]').should('be.visible');
+    cy.get('button[data-auth="logout"]').first().should('be.visible');
 
     // Now, log out
-    cy.get('button[data-auth="logout"]').click();
+    cy.get('button[data-auth="logout"]').first().click();
 
     // Verify logout was successful
-    cy.get('button[data-auth="login"]').should('be.visible');
+    cy.get('button[data-auth="login"]').first().should('be.visible');
   });
 });
